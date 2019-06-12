f4a48d4d90a54d2059f1d309c3715c1d46df147d
Sebastian Ortiz <49884961+sebas1799@users.noreply.github.com>
2019-06-12 17:54:30 -0500

    Update Numerotriangular.py

--- Numerotriangular.py
+++ Numerotriangular.py
@@ -1,5 +1,3 @@
 d=[(1,1),(2,3),(10,4),(15,5),(16,17),(21,6)]
-#print map(lambda x,y: [x]+[y[0]],map(lambda x: x/2 ,map(lambda x,y: x*y,map(lambda x: x[1]+1,d),map(lambda x: x[1],d))),d))
 print reduce(lambda x,y: x+y,map(lambda x: x[0],filter(lambda x: x[0]==(((x[1])*(x[1]+1))/2), d))) 
 
-"[x[1]]*[x[1]]+1/2"
